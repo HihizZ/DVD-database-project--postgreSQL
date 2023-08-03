103fac863bffa496320e87c601f291f60719700e
HihizZ <chongchishing@gmail.com>
2023-08-03 12:43:57 -0400

    update query

---
+++ Query05/Query04-JOIN_GroupBY_Oderby_SUM_AS.sql
@@ -0,0 +1,7 @@
+--find out the high value customer
+--SELECT Syntax Join, GROUP BY, Order by, SUM, AS 
+SELECT c.first_name, c.last_name, SUM(p.amount) AS total_amount
+FROM customer c
+JOIN payment p ON c.customer_id = p.customer_id
+GROUP BY c.first_name, c.last_name
+ORDER BY total_amount DESC;
